261ccacf90ccb195af2eb5e91bbffbd8a0550038
citizenvsl <victorsagristalopez@gmail.com>
2016-09-04 12:38:18 +0700

    Basic structure ready, electron working

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+var mainWindow = null;
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    height: 600,
+    width: 800
+  });
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+});
