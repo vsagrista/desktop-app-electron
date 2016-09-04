43184166bc4f6208af3f31123b6d52b15ccb0958
citizenvsl <victorsagristalopez@gmail.com>
2016-09-04 16:41:13 +0700

    Closing functionality with icp module ready

--- app/js/index.js
+++ app/js/index.js
@@ -1,20 +1,26 @@
 'use strict';
 
 var soundButtons = document.querySelectorAll('.button-sound');
+var ipcRenderer = require('electron').ipcRenderer;
+var closeEl = document.querySelector('.close');
 
 for (var i = 0; i < soundButtons.length; i++) {
-    var soundButton = soundButtons[i];
-    var soundName = soundButton.attributes['data-sound'].value;
+  var soundButton = soundButtons[i];
+  var soundName = soundButton.attributes['data-sound'].value;
 
-    prepareButton(soundButton, soundName);
+  prepareButton(soundButton, soundName);
 }
 
 function prepareButton(buttonEl, soundName) {
-    buttonEl.querySelector('span').style.backgroundImage = 'url("img/icons/' + soundName + '.png")';
+  buttonEl.querySelector('span').style.backgroundImage = 'url("img/icons/' + soundName + '.png")';
 
-    var audio = new Audio(__dirname + '/wav/' + soundName + '.wav');
-    buttonEl.addEventListener('click', function () {
-        audio.currentTime = 0;
-        audio.play();
-    });
+  var audio = new Audio(__dirname + '/wav/' + soundName + '.wav');
+  buttonEl.addEventListener('click', function () {
+      audio.currentTime = 0;
+      audio.play();
+  });
 }
+
+closeEl.addEventListener('click', function () {
+  ipcRenderer.send('close-main-window');
+});
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const {app, BrowserWindow} = electron
 
 var mainWindow = null;
+var ipcMain = electron.ipcMain;
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({
@@ -12,4 +13,9 @@ app.on('ready', function() {
     width: 368
   });
   mainWindow.loadURL('file://' + __dirname + '/app/index.html');
+});
+
+ipcMain.on('close-main-window', function () {
+  console.log('Bye bye!');
+  app.quit();
 });
